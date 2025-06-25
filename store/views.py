09aa1b2890742c441cce193330bf117d5b546757
AlgoClassWork <kyxec.play@gmail.com>
2025-06-25 13:05:02 +0600

    Update views.py

--- store/views.py
+++ store/views.py
@@ -4,6 +4,7 @@ from store.models import Product, Category
 
 # Create your views here.
 # http://127.0.0.1:8000/ главная
+# http://127.0.0.1:8000/category/3 например электроника
 def product_list(request, category_id=None):
     products = Product.objects.all()
     categories = Category.objects.all()
@@ -15,3 +16,9 @@ def product_list(request, category_id=None):
 
     return render(request, 'product_list.html', 
     {'products' : products, 'categories' : categories})
+
+# http://127.0.0.1:8000/product/1 например Iphone 15
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, 'product_detail.html',
+    {'product': product})
