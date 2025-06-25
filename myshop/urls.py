ee5ac29cc5ab11caa838c6999598c3a2f934f786
AlgoClassWork <kyxec.play@gmail.com>
2025-06-25 13:05:19 +0600

    Update urls.py

--- myshop/urls.py
+++ myshop/urls.py
@@ -19,14 +19,16 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
-from store.views import product_list
+from store.views import product_list, product_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # http://127.0.0.1:8000/ главная 
     path('', product_list, name='product_list'),
     # http://127.0.0.1:8000/category/3 например электроника
-    path('category/<int:category_id>/', product_list, name='category_products')
+    path('category/<int:category_id>/', product_list, name='category_products'),
+    # http://127.0.0.1:8000/product/1 например Iphone 15
+    path('product/<int:product_id>/', product_detail, name='product_detail'),
 ]   
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
