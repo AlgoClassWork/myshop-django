a79bbc80b8a4d4e57e607ed256f1d2abd4a1551e
AlgoClassWork <kyxec.play@gmail.com>
2025-06-26 12:47:06 +0600

    Update views.py

--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from store.models import Product, Category
 
@@ -22,3 +22,22 @@ def product_detail(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     return render(request, 'product_detail.html',
     {'product': product})
+
+# http://127.0.0.1:8000/cart Просмотр корзины
+def cart(request):
+    cart = request.session.get('cart', [])
+    products = Product.objects.filter(id__in=cart)
+    return render(request, 'cart.html', 
+    {'products': products})
+
+# http://127.0.0.1:8000/cart/add/2 Добавить Iphone 16
+def add_to_cart(request, product_id):
+    cart = request.session.get('cart', [])
+    if product_id not in cart:
+        cart.append(product_id)
+        request.session['cart'] = cart
+    return redirect('cart')
+
+# http://127.0.0.1:8000/cart/remove/3 Удалить Бетмена
+def remove_from_cart():
+    pass
