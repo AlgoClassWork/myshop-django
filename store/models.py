45bf819ddb92dbf80ef4a122c53d009bd334d878
AlgoClassWork <kyxec.play@gmail.com>
2025-06-27 12:13:42 +0600

    Update models.py

--- store/models.py
+++ store/models.py
@@ -16,3 +16,13 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Order(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    products = models.ManyToManyField(Product)
+
+    def __str__(self):
+        return f'Заказ #{self.id} от {self.name}'
