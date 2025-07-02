c60580c77202a40f5844f61295c6b14e591755f6
AlgoClassWork <kyxec.play@gmail.com>
2025-07-02 13:03:50 +0600

    Update models.py

--- store/models.py
+++ store/models.py
@@ -39,3 +39,13 @@ class Rating(models.Model):
 
     def __str__(self):
         return f' {self.user.username} - {self.product.title} : {self.score} '
+    
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f' {self.user.username} - {self.product.title} '
